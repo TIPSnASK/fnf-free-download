3ac0feddbcab3f23fb16cf87f82a39f6397f3c36
myo <85509162+cookiesock@users.noreply.github.com>
2024-07-08 08:41:16 -0500

    weeknd 2 started.

---
+++ data/characters/lady-weeknd2.hx
@@ -0,0 +1,52 @@
+// ffs
+static var ladySpeaker:FunkinSprite;
+static var speakerLightSpr:FunkinSprite;
+
+static var speakerAuto:Bool = true;
+static var speakerInterval:Int = 2;
+static var speakerLight:Bool = false;
+
+var skin:CustomShader;
+function postCreate() {
+	ladySpeaker = new FunkinSprite();
+	ladySpeaker.frames = Paths.getFrames("game/stages/speaker");
+	ladySpeaker.animation.add("hi", [0, 1, 2, 3], 0, true, false, false);
+	ladySpeaker.playAnim("hi", true);
+
+	speakerLightSpr = new FunkinSprite();
+	speakerLightSpr.frames = Paths.getFrames("game/stages/speaker-lights");
+	speakerLightSpr.animation.add("hi", [0, 3, 2, 1], 0, true, false, false);
+	speakerLightSpr.playAnim("hi", true);
+
+	shader = skin = new CustomShader("lady-colorswap");
+	applyPlayerSkin(skin, 'lady');
+}
+
+var firstFrame:Bool = true;
+function update(elapsed:Float) {
+	if (firstFrame) {
+		firstFrame = false;
+		state.insert(state.members.indexOf(this), ladySpeaker);
+		state.insert(state.members.indexOf(this), speakerLightSpr);
+	}
+	ladySpeaker.setPosition(x - (ladySpeaker.width*0.2), y + 77);
+	speakerLightSpr.setPosition(x - (speakerLightSpr.width*0.2), y);
+
+	speakerLightSpr.visible = speakerLight;
+}
+
+function beatHit(b) {
+	if (speakerAuto && b > 0) {
+		ladySpeaker.animation.curAnim.curFrame = speakerLightSpr.animation.curAnim.curFrame = FlxMath.wrap(Std.int(b/speakerInterval), 0, 3);
+	}
+}
+
+// ffs
+function destroy() {
+	ladySpeaker = null;
+	speakerLightSpr = null;
+
+	speakerAuto = null;
+	speakerInterval = null;
+	speakerLight = null;
+}
---
+++ data/stages/bus.hx
@@ -0,0 +1,25 @@
+import flixel.addons.display.FlxBackdrop;
+
+public var busSpeed:Float = 1; // there is literally no reason for this i just think its fun
+
+var sky:FunkinSprite;
+var backbustrees:FlxBackdrop;
+var backbus:FlxBackdrop;
+
+function create() {
+	backbus = new FlxBackdrop().loadGraphic(Paths.image('game/stages/bus/backbus'));
+	backbus.repeatAxes = 0x01;
+	backbus.y = 90;
+	insert(0, backbus);
+	backbus.velocity.x = -75*busSpeed;
+
+	backbustrees = new FlxBackdrop().loadGraphic(Paths.image('game/stages/bus/backbustrees'));
+	backbustrees.repeatAxes = 0x01;
+	backbustrees.y = 82;
+	insert(0, backbustrees);
+	backbustrees.velocity.x = (backbus.velocity.x*0.25)*busSpeed;
+
+	insert(0, sky = new FunkinSprite().makeGraphic(FlxG.width, FlxG.height, 0xFFC0C0C0));
+	sky.scrollFactor.set();
+	sky.zoomFactor = 0;
+}
