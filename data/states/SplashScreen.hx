3c30bbf31d1953661315ff9a34ad223d88f2f006
myo <85509162+cookiesock@users.noreply.github.com>
2024-07-08 15:59:13 -0500

    new splash screen by JSlimSlam

--- data/states/SplashScreen.hx
+++ data/states/SplashScreen.hx
@@ -4,6 +4,8 @@ import funkin.backend.MusicBeatState;
 function create() {
 	var _splashList:Array<String> = [for (_i in Paths.getFolderContent('images/splash-screens')) Path.withoutExtension(_i)];
 	var splash:FunkinSprite = new FunkinSprite().loadGraphic(Paths.image('splash-screens/${_splashList[FlxG.random.int(0, _splashList.length-1)]}'));
+	splash.setGraphicSize(400, 400);
+	splash.updateHitbox();
 	add(splash);
 
 	new FlxTimer().start(2, function() {
