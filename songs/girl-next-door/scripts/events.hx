2dd298bf87e530dfcc3831317ef8d0fcf5863833
myo <85509162+cookiesock@users.noreply.github.com>
2024-07-10 09:48:09 -0500

    Update events.hx

--- songs/girl-next-door/scripts/events.hx
+++ songs/girl-next-door/scripts/events.hx
@@ -1,6 +1,6 @@
-var ladyDance:FunkinSprite;
-var dudeDance:FunkinSprite;
+// var ladyDance:FunkinSprite;
+// var dudeDance:FunkinSprite;
 
-function postCreate() {
-	ladyDance = new FunkinSprite();
-}
+// function postCreate() {
+// 	ladyDance = new FunkinSprite();
+// }
